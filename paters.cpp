cb0293b607c0e443e7096249d99ee20f57d889aa
devashishproject <83362314+devashishproject@users.noreply.github.com>
2022-09-28 21:01:54 +0530

    Create paters.cpp

---
+++ paters.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   int i=1;
+   cin>>n;
+   while(i<=n){
+       
+       int space=i-1;
+       while(space){
+           cout<<" ";
+          space=space-1;
+       } 
+       int j=1;
+       
+       while(j<=n-i+1){
+           //int star=n-i+1;
+            
+           cout<<"*";
+          
+          j=j+1;
+           
+       }
+       cout<<endl;
+       i=i+1;
+   }
+
+    return 0;
+}
